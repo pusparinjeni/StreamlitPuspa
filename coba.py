ea0bbcf56c9ac4b36c9d7910bb7e588414cd5789
pusparinjeni <40913994+pusparinjeni@users.noreply.github.com>
2022-05-18 07:47:23 +0700

    commit

--- coba.py
+++ coba.py
@@ -2,7 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Warung di Surabaya')
+primaryColor = "#d33682"
+backgroundColor = "#002b36"
+secondaryBackgroundColor = "#586e75"
+textColor = "#fafafa"
+font = "sans serif"
 
 
 #Mengambil Data
@@ -12,12 +16,17 @@ def get_data():
 
 
 dataready = get_data()
-#st.table(dataready.head())
-st.table(dataready[['Name', 'Kecamatan','Rating','Reviews']].head())
+
+
+st.title("Surabaya Halal Directory")
+
+st.write('Halo Arek Suroboyo? Apakah anda pecinta kuliner? Pernahkah anda berfikir kuliner yang disajikan sudah tersertifikasi halal? Sebagai seorang muslim memang suatu kebutuhan dan diwajibkan memakanan makanan dan minuman halal, baik dari bahan dan cara pengolahannya. Kalau anda penasaran kuliner yang biasanya anda makan halal atau tidak. Aplikasi ini membantu mengecek sertifikasi halal warung/restaurant/warung/bakery dan kuliner lainnya di Kota Surabaya. Aplikasi ini menggabungkan data google maps dan data SIHALAL BPJH menggunakan algoritma Jaccad Similarity.')
+
+st.write('Anda dapat menemukan source dan dataset disini(https: // github.com/adeindriawan/inovboyo)')
 
 #Mengurutkan berdasarkan review
-st.header("Warung dengan rating tertinggi")
-values = st.slider("Rating", 1.0, 5.0, step=1.0)
+st.header("Rating Warung/Restaurant di Surabaya")
+values = st.slider("Rating", 1.0, 5.0, step=0.1)
 dataready.round({'Rating':1})
 table_rating = dataready[['Name', 'Rating']].sort_values(by=['Rating'], ascending=False)
 st.table(table_rating[table_rating['Rating'] == values].style.format({"Rating": "{:.2f}"}))
@@ -26,10 +35,18 @@ st.table(table_rating[table_rating['Rating'] == values].style.format({"Rating": 
 #Mengurutkan berdasarkan rating
 st.header("Warung yang paling banyak direview")
 values2 = st.slider("Reviews", 5, 20, step=5)
-dataready['Reviews'].astype('float')
+#dataready['Reviews'].astype('float')
 st.table(dataready[['Name', 'Reviews']].sort_values(by=['Reviews'], ascending=False).head(values2))
 
-st.header("Jumlah Warung per Kecamatan")
-values3 = st.slider("Jumlah", 5, 20, step=5)
-st.table(dataready[['Kecamatan', 'Name']].groupby(["Kecamatan"]).count().sort_values(
-    "Name", ascending=False).head(values3))
+#Jumlah Warung per kecamatan
+#st.header("Jumlah Warung per Kecamatan")
+#values3 = st.slider("Jumlah", 5, 20, step=5)
+#st.table(dataready[['Kecamatan', 'Name']].groupby(["Kecamatan"]).count().sort_values("Name", ascending=False).head(values3))
+
+#Jumlah Cabang berdasarkan website
+st.header("Jumlah Cabang")
+values4 = st.slider("Jumlah Cabang", 5, 20, step=5)
+st.table(dataready[['Website', 'Name']].groupby(
+    ["Website"]).count().sort_values("Name", ascending=False).head(values4))
+
+
